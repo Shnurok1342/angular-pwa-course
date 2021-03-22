beb3f825e0aebdabb6a6af3bdf2d29e8efb8d407
Shnurok1342 <a13bepb@yandex.ru>
2021-03-22 14:11:20 +0300

    (Push Notifications) feat: Server Identification - Generating a VAPID key with the web-push library

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,52 +1,48 @@
 import {Component, OnInit} from '@angular/core';
-import {LessonsService} from "../services/lessons.service";
+import {LessonsService} from '../services/lessons.service';
 import {Observable, of} from 'rxjs';
-import {Lesson} from "../model/lesson";
-import {SwPush} from "@angular/service-worker";
-import {NewsletterService} from "../services/newsletter.service";
+import {Lesson} from '../model/lesson';
+import {SwPush} from '@angular/service-worker';
+import {NewsletterService} from '../services/newsletter.service';
 import {catchError} from 'rxjs/operators';
 
 @Component({
-    selector: 'lessons',
-    templateUrl: './lessons.component.html',
-    styleUrls: ['./lessons.component.css']
+  selector: 'lessons',
+  templateUrl: './lessons.component.html',
+  styleUrls: ['./lessons.component.css']
 })
 export class LessonsComponent implements OnInit {
+  lessons$: Observable<Lesson[]>;
+  isLoggedIn$: Observable<boolean>;
 
-    lessons$: Observable<Lesson[]>;
-    isLoggedIn$: Observable<boolean>;
+  readonly VAPID_PUBLIC_KEY = 'BHeD-NBOy8n7QXqjp2p2yCZSGje7bj4vLDDZQaWo3mn2LGW0g7VeAprcBuJInQdu8IetufgIcCywlt73OnnJEwg';
 
-    readonly VAPID_PUBLIC_KEY = "TODO";
+  constructor(
+    private lessonsService: LessonsService,
+    private swPush: SwPush,
+    private newsletterService: NewsletterService
+  ) {}
 
-    constructor(
-        private lessonsService: LessonsService,
-        private newsletterService: NewsletterService) {
+  ngOnInit() {
+    this.loadLessons();
+  }
 
+  loadLessons() {
+    this.lessons$ = this.lessonsService.loadAllLessons().pipe(catchError(err => of([])));
+  }
+
+  subscribeToNotifications() {
+    if (this.swPush.isEnabled) {
+      this.swPush.requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY
+      })
+        .then(sub => {
+          console.log('Notification Subscription: ', sub);
+        })
+        .catch(err => console.log('Could not subscribe to notifications', err));
     }
+  }
 
-    ngOnInit() {
-        this.loadLessons();
-    }
-
-
-    loadLessons() {
-        this.lessons$ = this.lessonsService.loadAllLessons().pipe(catchError(err => of([])));
-    }
-
-    subscribeToNotifications() {
-
-
-
-    }
-
-
-    sendNewsletter() {
-
-
-    }
-
-
-
-
-
+  sendNewsletter() {
+  }
 }
