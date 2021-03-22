2c4703dd34ab16f192b62cb0df192d8c4eb5b5ed
Shnurok1342 <a13bepb@yandex.ru>
2021-03-22 19:33:02 +0300

    (Service Worker) feat: Service Workers and Application Versioning - Install & Activate Lifecycle Phases

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,15 @@
-const VERSION = 'v1';
+const VERSION = 'v2';
 function log(messages) {
   console.log(VERSION, messages);
 }
 
 log('Installing Service Worker');
+
+self.addEventListener('install', () => {
+  log('Version is installed');
+
+});
+
+self.addEventListener('activate', () => {
+  log('Version is activated');
+});
