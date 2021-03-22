53e5ca01c33728f7258c21b15521390c26df7e1f
Shnurok1342 <a13bepb@yandex.ru>
2021-03-22 19:55:30 +0300

    (Service Worker) feat: Downloading The Offline Page - The Service Worker Installation Phase

--- src/sw.js
+++ src/sw.js
@@ -1,15 +1,27 @@
-const VERSION = 'v2';
-function log(messages) {
-  console.log(VERSION, messages);
-}
+const VERSION = 'v3';
 
 log('Installing Service Worker');
 
-self.addEventListener('install', () => {
-  log('Version is installed');
-
-});
+self.addEventListener('install', event => event.waitUntil(installServiceWorker()));
 
 self.addEventListener('activate', () => {
-  log('Version is activated');
+  log('version is activated');
 });
+
+async function installServiceWorker() {
+  log("Service Worker installation started ");
+  const request = new Request('offline.html');
+  const response = await fetch(request);
+  log("response received after loading offline.html", response);
+  if (response.status !== 200) {
+    throw new Error('Could not load offline page!');
+  }
+}
+
+function log(message, ...data) {
+  if (data.length > 0) {
+    console.log(VERSION, message, data);
+  } else {
+    console.log(message);
+  }
+}
